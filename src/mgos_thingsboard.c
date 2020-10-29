db9084a46bf7bc41a4b2af5d795a053647789e65
suyashmathema <suyashmathema@gmail.com>
2020-10-29 09:43:02 +0545

    Added publish telemetry

--- src/mgos_thingsboard.c
+++ src/mgos_thingsboard.c
@@ -2,17 +2,28 @@
 #include "mgos_mqtt.h"
 
 #define TBP_EVENT_BASE MGOS_EVENT_BASE('T', 'B', 'P')
+#define TBP_TELEMETRY_TIMED (1 << 0)
+#define TBP_TELEMETRY_DELAYED (1 << 1)
 
 const char* ATTR_PUB_TOPIC = "v1/devices/me/attributes";
 const char* ATTR_REQ_PUB_TOPIC = "v1/devices/me/attributes/request/1";
 const char* ATTR_UPDATE_SUB_TOPIC = "v1/devices/me/attributes";
 const char* ATTR_RESP_SUB_TOPIC = "v1/devices/me/attributes/response/+";
+const char* TELE_PUB_TOPIC = "v1/devices/me/telemetry";
 
 enum tb_event {
     TB_INITIALIZED = TBP_EVENT_BASE,
     TB_ATTRIBUTE_UPDATE,
     TB_ATTRIBUTE_RESPONSE,
 };
+
+struct mgos_thingsboard_config {
+    bool user_active;
+    mgos_timer_id tele_delay_timer;
+    char* delayed_telemetry;
+};
+
+struct mgos_thingsboard_config tb_config = {.user_active = false, .tele_delay_timer = 0};
 
 uint16_t tb_request_attributes(const char* client_keys, const char* shared_keys) {
     uint16_t res = 0;
@@ -28,27 +39,90 @@ uint16_t tb_request_attributes(const char* client_keys, const char* shared_keys)
     return res;
 }
 
-uint16_t tb_publish_client_attributes() {
-    LOG(LL_INFO, ("tb_publish_client_attributes - publishing client attributes"));
+uint16_t tb_publish_config_attributes() {
+    LOG(LL_INFO, ("tb_publish_config_attributes - publishing client attributes"));
     struct mbuf msg_mbuf;
     mbuf_init(&msg_mbuf, 0);
     mgos_conf_emit_cb(&mgos_sys_config, NULL, mgos_config_schema_tb_client(), true, &msg_mbuf, NULL, NULL);
-    uint16_t res = mgos_mqtt_pub(ATTR_PUB_TOPIC, msg_mbuf.buf, msg_mbuf.len, 0, false);
+    uint16_t res = mgos_mqtt_pub(ATTR_PUB_TOPIC, msg_mbuf.buf, msg_mbuf.len, 1, false);
     mbuf_free(&msg_mbuf);
     return res;
 }
 
-uint16_t tb_publish_client_attributesf(const char* json_fmt, ...) {
-    uint16_t res;
+uint16_t tb_publish_attributes(const char* attributes, int attributes_len) {
+    return mgos_mqtt_pub(ATTR_PUB_TOPIC, attributes, attributes_len, 1, false);
+}
+
+uint16_t tb_publish_attributesf(const char* json_fmt, ...) {
+    uint16_t res = 0;
     va_list ap;
     va_start(ap, json_fmt);
-    res = mgos_mqtt_pubv(ATTR_PUB_TOPIC, 0, false, json_fmt, ap);
+    res = mgos_mqtt_pubv(ATTR_PUB_TOPIC, 1, false, json_fmt, ap);
     va_end(ap);
     return res;
 }
 
-uint16_t tb_publish_client_attributesv(const char* json_fmt, va_list ap) {
-    return mgos_mqtt_pubv(ATTR_PUB_TOPIC, 0, false, json_fmt, ap);
+uint16_t tb_publish_attributesv(const char* json_fmt, va_list ap) {
+    return mgos_mqtt_pubv(ATTR_PUB_TOPIC, 1, false, json_fmt, ap);
+}
+
+static void pub_telemetry_cb(void* arg) {
+    LOG(LL_INFO, ("pub_telemetry_cb - publishing delayed telemetry %.*s",
+                  strlen(tb_config.delayed_telemetry), tb_config.delayed_telemetry));
+    mgos_mqtt_pub(TELE_PUB_TOPIC, tb_config.delayed_telemetry, strlen(tb_config.delayed_telemetry), 1, false);
+    if (tb_config.delayed_telemetry != NULL) {
+        free(tb_config.delayed_telemetry);
+        tb_config.delayed_telemetry = NULL;
+    }
+    mgos_clear_timer(tb_config.tele_delay_timer);
+    tb_config.tele_delay_timer = 0;
+    (void)arg;
+}
+
+uint16_t tb_publish_telemetry(int flags, unsigned int time, const char* telemetry, int telemetry_len) {
+    uint16_t res = 0;
+    if (flags & TBP_TELEMETRY_TIMED) {
+        if (time == 0) {
+            time = mgos_uptime_micros() / 1000;
+        }
+        char* timed_telemetry = json_asprintf("{ts:%d, values:%.*s}", time, telemetry_len, telemetry);
+        LOG(LL_INFO, ("tb_publish_telemetry - published timed telemetry %.*s", strlen(timed_telemetry), timed_telemetry));
+        res = mgos_mqtt_pub(TELE_PUB_TOPIC, timed_telemetry, strlen(timed_telemetry), 1, false);
+        free(timed_telemetry);
+    } else if (flags & TBP_TELEMETRY_DELAYED) {
+        if (tb_config.delayed_telemetry != NULL) {
+            free(tb_config.delayed_telemetry);
+            tb_config.delayed_telemetry = NULL;
+        }
+        mgos_clear_timer(tb_config.tele_delay_timer);
+        tb_config.tele_delay_timer = 0;
+
+        tb_config.delayed_telemetry = strndup(telemetry, telemetry_len);
+        tb_config.tele_delay_timer = mgos_set_timer(5000, 0, pub_telemetry_cb, NULL);
+    } else {
+        LOG(LL_INFO, ("tb_publish_telemetry - published telemetry %.*s", telemetry_len, telemetry));
+        res = mgos_mqtt_pub(TELE_PUB_TOPIC, telemetry, telemetry_len, 1, false);
+    }
+    return res;
+}
+
+uint16_t tb_publish_telemetryf(int flags, unsigned int time, const char* telemetry_fmt, ...) {
+    uint16_t res = 0;
+    va_list ap;
+    va_start(ap, telemetry_fmt);
+    char* telemetry = json_vasprintf(telemetry_fmt, ap);
+    res = tb_publish_telemetry(flags, time, telemetry, strlen(telemetry));
+    free(telemetry);
+    va_end(ap);
+    return res;
+}
+
+uint16_t tb_publish_telemetryv(int flags, unsigned int time, const char* telemetry_fmt, va_list ap) {
+    uint16_t res = 0;
+    char* telemetry = json_vasprintf(telemetry_fmt, ap);
+    res = tb_publish_telemetry(flags, time, telemetry, strlen(telemetry));
+    free(telemetry);
+    return res;
 }
 
 uint16_t tb_sync_shared_attributes() {
@@ -106,12 +180,42 @@ static void mqtt_event_handler(struct mg_connection* nc, int ev, void* ev_data, 
     if (ev == MG_EV_MQTT_CONNACK) {
         LOG(LL_INFO, ("mqtt_event_handler - MQTT connection acknowledge"));
         tb_sync_shared_attributes();
-        tb_publish_client_attributes();
+        tb_publish_config_attributes();
     }
 }
 
+int btn_idx = 0;
 void btn_cb(int pin, void* arg) {
-    tb_sync_shared_attributes();
+    // tb_sync_shared_attributes();
+    LOG(LL_INFO, ("btn_cb - Button pressed count: %d", btn_idx));
+
+    char* a;
+    char* b;
+    char* c;
+
+    switch (btn_idx % 5) {
+        case 0:
+            a = "{\"temp\":23.43,\"hum\":122,\"address\":\"lalitpur\"}";
+            tb_publish_telemetry(TBP_TELEMETRY_DELAYED, 0, a, strlen(a));
+            break;
+        case 1:
+            b = "{\"temp\":35.23,\"hum\":98,\"address\":\"patan\"}";
+            tb_publish_telemetry(TBP_TELEMETRY_TIMED, 0, b, strlen(b));
+            break;
+        case 2:
+            c = "{\"temp\":33.43,\"hum\":222,\"address\":\"kalimati\"}";
+            tb_publish_telemetry(0, 0, c, strlen(c));
+            break;
+        case 3:
+            tb_publish_telemetryf(0, 0, "{temp:%f,hum:%d,address:%s}", 4.23, 99, "kathmandu");
+            break;
+        case 4:
+            tb_publish_telemetryf(TBP_TELEMETRY_DELAYED, 0, "{temp:%d,hum:%d,address:%s}", 423, 99, "kathmandu");
+            break;
+        default:
+            break;
+    }
+    btn_idx++;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
